1878de457696f86900f462b450651a4d4a3d337b
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-05-11 18:30:32 -0600

    addes structure and functionality to quiz using CSS, HTM, and JS

---
+++ develop/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the file extension for a JavaScript file?",
+        answer: ".js",
+        options: [
+            ".java",
+            ".javascript",
+            ".js",
+            ".com"
+        ]
+    },
+    {
+        numb: 2,
+        question: "In what year was JavaScript invented?",
+        answer: 1995,
+        options: [
+            1995,
+            2001,
+            1993,
+            2005
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the correct syntax for creating a variable in Javascript?",
+        answer: "var myVariable",
+        options: [
+            "var = myVariable;",
+            "var myVariable;",
+            "let myVariable",
+            "variable = myVariable;"
+        ]
+    },
+    {
+        numb: 4,
+        question: "which keyword is used to declare a function in JavaScript?",
+        answer: "funtion",
+        options: [
+            "let",
+            "execute",
+            "operate",
+            "function"
+        ]
+    },
+    {
+        numb: 5,
+        question: "what is the output of the follwing code snippet: console.log(typeof 42)",
+        answer: "number",
+        options: [
+            "number",
+            "42",
+            "forty-two",
+            "4,2",
+        ]
+    },
+    {
+        numb: 6,
+        question: "How do you add a comment in JavaScript?",
+        answer: "// This is a comment",
+        options: [
+            "/* this is a comment */",
+            "<!-- This is a comment -->",
+            "#this is a comment",
+            "// This is a comment"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the result of the expression '5' + 2 in javaScript",
+        answer: "52",
+        options: [
+            "7",
+            "5+2",
+            "52",
+            "'5',2"
+        ]
+    },
+    {
+        numb: 8,
+        question: "How can you check the length of a string in JavaScript?",
+        answer: "By using the property: myString.length",
+        options: [
+            "by using the property: console.log(string.length(total)",
+            "print(string.length)",
+            "By using the property: myString.length",
+            "function(string_length)"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the difference between == and === in JavaScript?",
+        answer: "== means same type, while === requires both type and value to be equal",
+        options: [
+            "== performs type checking, while === does not check types.",
+            "== means same type, while === requires both type and value to be equal",
+            "=== performs type coercion, while == does not coerce types.",
+            "== compares values and types, while === only compares values."
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the purpose of the 'this' keyword in Javascript?",
+        answer: "It refers to the current object or context.",
+        options: [
+            "It refers to the current object or context.",
+            "it refers to gloabl elements",
+            "links JavaScript file to HTML",
+            "toothbrush"
+        ]
+    }
+]
